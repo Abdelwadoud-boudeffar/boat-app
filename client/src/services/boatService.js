1e1e1baeca26941bec541171a2b53343594e5f2c
aewboudeffar <aewboudeffar@sqli.com>
2021-07-14 09:34:18 +0200

    url ip address to localhost

--- client/src/services/boatService.js
+++ client/src/services/boatService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://192.168.1.24:8088/api/v1',
+  baseURL: 'http://localhost:8088/api/v1',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
